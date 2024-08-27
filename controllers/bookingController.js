3153a47610d102b5ce7dc10569a60f69c8b10ade
Hardik Parikh <hardikparikh19@gmail.com>
2024-08-27 20:36:38 +0530

    Added some logging for debugging

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,6 +60,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
+  console.log('Session object:', session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
 
